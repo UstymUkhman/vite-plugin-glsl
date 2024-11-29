e3e6b3c15829d980a116a7c4a82e1d55c7bead60
Ustym Ukhman <ustym.ukhman@gmail.com>
2024-11-29 13:57:26 +0200

    Update Plugin Version

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
  * @module vite-plugin-glsl
  * @author Ustym Ukhman <ustym.ukhman@gmail.com>
  * @description Import, inline (and compress) GLSL shader files
- * @version 1.3.0
+ * @version 1.3.1
  * @license MIT
  */
 
