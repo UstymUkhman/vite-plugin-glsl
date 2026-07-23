1f513210b9c5e27905b09de00d91d63360b80725
Philip Garrison <philip.garrison@alleninstitute.org>
2026-07-23 14:49:18 -0700

    Add test case for line comments inside block comments

--- test/automated/comment-delimiters.test.js
+++ test/automated/comment-delimiters.test.js
@@ -26,6 +26,26 @@ test('loadShader ignores block-comment delimiters inside line comments', async (
   assert.equal(outputShader, 'void main() {}');
 });
 
+// Similarly, line comments must be interpreted in block comment context
+test('loadShader ignores line comments inside block comments', async () => {
+  const source = [
+    '/* commentedOut(code) // explanation of dead code */',
+    'void main() {}'
+  ].join('\n');
+
+  const { outputShader } = await loadShader(source, '/virtual/shader.frag', {
+    removeDuplicatedImports: false,
+    warnDuplicatedImports: true,
+    defaultExtension: 'glsl',
+    importKeywords: ['#include'],
+    onComplete: undefined,
+    minify: false,
+    root: '/'
+  });
+
+  assert.equal(outputShader, 'void main() {}');
+});
+
 test('loadShader rejects unterminated block comments', async () => {
   const source = [
     'precision highp float;',
